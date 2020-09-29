c3dd55b7327cc76693b70f9f528b04a9b939d78d
AlekseyDanchin <aleksey.danchin@gmail.com>
2020-09-29 20:11:09 +0300

    Frontend

--- front/src/index.js
+++ front/src/index.js
@@ -1,4 +1,49 @@
 const socket = io();
 
-socket.emit("message", "Привет мир!", { a: 5 });
-socket.emit("abra", "cadabra");
+import Chat from "./Chat";
+
+const chat = new Chat();
+
+document.body.append(chat.view);
+
+chat.addEventListener("send", (message) => {
+	message = message.trim();
+
+	if (!message) {
+		return;
+	}
+
+	if (message.startsWith("/setname ")) {
+		const split = message.split(" ");
+		const name = split[1].slice(0, 10);
+
+		socket.emit("setname", name);
+		chat.clearInput();
+		return;
+	}
+
+	socket.emit("message", message);
+	chat.clearInput();
+});
+
+socket.on("message", (data) => {
+	chat.addMessage(data);
+});
+
+// chat.addMessage({
+// 	user: "Сергея Воронина",
+// 	date: Date.now(),
+// 	content: "Привет, други!",
+// });
+
+// chat.addMessage({
+// 	user: "Татьяна Ошка",
+// 	date: Date.now(),
+// 	content: "Всем добрый вечер!",
+// });
+
+// chat.addMessage({
+// 	user: "Сергея Воронина",
+// 	date: Date.now(),
+// 	content: "Привет, други!",
+// });
